53598c43234e387e1872ee41596d2be4d0881712
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2023-09-04 19:47:52 -0500

    initial

---
+++ code/sequences_to_sequences_model.py
@@ -0,0 +1,216 @@
+###################################################
+# Overview
+#
+# Main script. Trains two sequence to sequence models.
+#
+# Code follows Deep Learning with Python by Fancois Chollet.
+# Second edition.
+###################################################
+
+# %%
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from transformer_classes import TransformerEncoder, PositionalEmbedding, TransformerDecoder
+import os
+import random
+import string
+import re
+
+# %%
+os.chdir('S:/Python/projects/translator')
+
+# %%
+keras.mixed_precision.set_global_policy("mixed_float16")
+
+###############################
+# Create starting dataset
+###############################
+
+# %%
+text_file = os.getcwd() + "\\data\\spa.txt"
+with open(text_file) as f:
+    lines = f.read().split("\n")[:-1]
+text_pairs = []
+
+# %% randomly inspect
+for line in lines:
+    english, spanish = line.split("\t")
+    spanish = "[start] " + spanish + " [end]"
+    text_pairs.append((english, spanish))
+print(random.choice(text_pairs))
+
+# %% randomply split
+random.shuffle(text_pairs)
+num_val_samples = int(0.15 * len(text_pairs))
+num_train_samples = len(text_pairs) - 2 * num_val_samples
+train_pairs = text_pairs[:num_train_samples]
+val_pairs = text_pairs[num_train_samples:num_train_samples + num_val_samples]
+test_pairs = text_pairs[num_train_samples + num_val_samples:]
+
+# %%
+strip_chars = string.punctuation + "¿"
+strip_chars = strip_chars.replace("[", "")
+strip_chars = strip_chars.replace("]", "")
+
+def custom_standardization(input_string):
+    lowercase = tf.strings.lower(input_string)
+    return tf.strings.regex_replace(lowercase, f"[{re.escape(strip_chars)}]", "")
+
+vocab_size = 15000
+sequence_length = 20
+
+source_vectorization = layers.TextVectorization(max_tokens=vocab_size, output_mode="int", output_sequence_length=sequence_length)
+target_vectorization = layers.TextVectorization(max_tokens=vocab_size, output_mode="int", output_sequence_length=sequence_length + 1, 
+                                                standardize=custom_standardization)
+train_english_texts = [pair[0] for pair in train_pairs]
+train_spanish_texts = [pair[1] for pair in train_pairs]
+source_vectorization.adapt(train_english_texts)
+target_vectorization.adapt(train_spanish_texts)
+
+# %%
+# Book has batch size 64
+# Runs a bit faster with larger batch sizes. 
+# Getting similar accuracy.
+batch_size = 1024 # Book has 64
+
+def format_dataset(eng, spa):
+    eng = source_vectorization(eng)
+    spa = target_vectorization(spa)
+    return ({"english": eng, "spanish": spa[:, :-1]}, spa[:, 1:])
+
+def make_dataset(pairs):
+    eng_texts, spa_texts = zip(*pairs)
+    eng_texts = list(eng_texts)
+    spa_texts = list(spa_texts)
+    dataset = tf.data.Dataset.from_tensor_slices((eng_texts, spa_texts))
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.map(format_dataset, num_parallel_calls=4)
+    dataset = dataset.shuffle(2048).prefetch(16)
+    return dataset
+
+train_ds = make_dataset(train_pairs)
+val_ds = make_dataset(val_pairs)
+
+###############################
+# GRU
+###############################
+# %%
+embed_dim = 256
+latent_dim = 1024
+
+strategy = tf.distribute.MirroredStrategy()
+
+with strategy.scope():
+
+    source = keras.Input(shape=(None,), dtype="int64", name="english")
+    x = layers.Embedding(vocab_size, embed_dim, mask_zero=True)(source)
+    encoded_source = layers.Bidirectional(layers.GRU(latent_dim), merge_mode="sum")(x)
+
+    past_target = keras.Input(shape=(None,), dtype="int64", name="spanish")
+    x = layers.Embedding(vocab_size, embed_dim, mask_zero=True)(past_target)
+    decoder_gru = layers.GRU(latent_dim, return_sequences=True)
+    x = decoder_gru(x, initial_state=encoded_source)
+    x = layers.Dropout(0.5)(x)
+    target_next_step = layers.Dense(vocab_size, activation="softmax")(x)
+
+    seq2seq_rnn = keras.Model([source, past_target], target_next_step)
+    seq2seq_rnn.compile( optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+seq2seq_rnn.summary()
+
+# %% Epoch around 32 seconds batch size
+seq2seq_rnn.fit(train_ds, epochs=15, validation_data=val_ds) # Book got 64% accuracy on validation.
+
+###############################
+# Translate a few sentences
+###############################
+# %%
+spa_vocab = target_vectorization.get_vocabulary()
+spa_index_lookup = dict(zip(range(len(spa_vocab)), spa_vocab))
+max_decoded_sentence_length = 20
+
+def decode_sequence(input_sentence):
+    tokenized_input_sentence = source_vectorization([input_sentence])
+    decoded_sentence = "[start]"
+    for i in range(max_decoded_sentence_length):
+        tokenized_target_sentence = target_vectorization([decoded_sentence])
+        next_token_predictions = seq2seq_rnn.predict( [tokenized_input_sentence, tokenized_target_sentence])
+        sampled_token_index = np.argmax(next_token_predictions[0, i, :])
+
+        sampled_token = spa_index_lookup[sampled_token_index]
+        decoded_sentence += " " + sampled_token
+        if sampled_token == "[end]":
+            break
+    return decoded_sentence
+
+# %%
+test_eng_texts = [pair[0] for pair in test_pairs]
+for _ in range(20):
+    input_sentence = random.choice(test_eng_texts)
+    print("-")
+    print(input_sentence)
+    print(decode_sequence(input_sentence))
+
+###############################
+# Model: Transformer
+###############################
+
+# %%
+embed_dim = 256
+dense_dim = 2048
+num_heads = 8
+
+strategy = tf.distribute.MirroredStrategy()
+
+with strategy.scope():
+
+    encoder_inputs = keras.Input(shape=(None,), dtype="int64", name="english")
+    x = PositionalEmbedding(sequence_length, vocab_size, embed_dim)(encoder_inputs)
+    encoder_outputs = TransformerEncoder(embed_dim, dense_dim, num_heads)(x)
+
+    decoder_inputs = keras.Input(shape=(None,), dtype="int64", name="spanish")
+    x = PositionalEmbedding(sequence_length, vocab_size, embed_dim)(decoder_inputs)
+    x = TransformerDecoder(embed_dim, dense_dim, num_heads)(x, encoder_outputs)
+    x = layers.Dropout(0.5)(x)
+
+    decoder_outputs = layers.Dense(vocab_size, activation="softmax")(x)
+
+    transformer = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    transformer.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+transformer.summary()
+
+# %%
+transformer.fit(train_ds, epochs=30, validation_data=val_ds) # Book got 67% accuracy
+
+###############################
+# Translate a few sentences w/ transformer
+###############################
+# %%
+spa_vocab = target_vectorization.get_vocabulary()
+spa_index_lookup = dict(zip(range(len(spa_vocab)), spa_vocab))
+max_decoded_sentence_length = 20
+
+def decode_sequence(input_sentence):
+    tokenized_input_sentence = source_vectorization([input_sentence])
+    decoded_sentence = "[start]"
+    for i in range(max_decoded_sentence_length):
+        tokenized_target_sentence = target_vectorization([decoded_sentence])[:, :-1]
+        predictions = transformer([tokenized_input_sentence, tokenized_target_sentence])
+        sampled_token_index = np.argmax(predictions[0, i, :])
+        sampled_token = spa_index_lookup[sampled_token_index]
+        decoded_sentence += " " + sampled_token
+        if sampled_token == "[end]":
+            break
+    return decoded_sentence
+
+# %%
+test_eng_texts = [pair[0] for pair in test_pairs]
+for _ in range(20):
+    input_sentence = random.choice(test_eng_texts)
+    print("-")
+    print(input_sentence)
+    print(decode_sequence(input_sentence))
+
+# %%
